5fa13e663ae3008cbb0625610143d3bbc80f9d1d
Brett DeWoody <brettdewoody@gmail.com>
2020-12-06 10:56:13 +0000

    Add Day 6.

---
+++ days/06/index.js
@@ -0,0 +1,38 @@
+const readInput = require('../readInput')
+
+
+const solution = (data, validation) => {
+  const items = data.split(/\n\n/)
+  let total = 0
+
+  items.forEach((item) => {
+    const answers = item.replace(/\r?\n/g, '')
+    const questions = new Set(answers.split(''))
+    total += questions.size
+  })
+
+  return total
+}
+
+const solution2 = (data, validation) => {
+  const items = data.split(/\n\n/)
+  let total = 0
+
+  items.forEach((item) => {
+    const answers = item.split(/\r?\n/g).map((person) => person.split(''))
+    const result = answers.shift().reduce(function(res, v) {
+      if (res.indexOf(v) === -1 && answers.every(function(a) {
+          return a.indexOf(v) !== -1;
+      })) res.push(v);
+      return res;
+  }, []);
+    total += result.length
+  })
+
+  return total
+}
+
+const data = readInput('./input.txt').then((data) => {
+  console.log('Number of Yes: ', solution(data))
+  console.log('All Yes: ', solution2(data))
+})
